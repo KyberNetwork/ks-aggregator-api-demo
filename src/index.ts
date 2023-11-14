b278d79442f0fc0db2b3804a0d5f94e2b7b42fe5
Aw Kai Shin <kaishinaw@Aws-MacBook-Pro.local>
2023-11-14 14:01:56 +0800

    Add structure to index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { getSwapRouteEncodedV0 } from "./apis/V0Get";
 import { getSwapRouteV1 } from "./apis/V1Get";
 import { postSwapRouteV1 } from "./apis/V1Post";
-import { V0Swap } from "./operations/V0Swap";
 import { V1Swap } from "./operations/V1Swap";
+import { getSwapRouteEncodedV0 } from "./apis/V0Get";
+import { V0Swap } from "./operations/V0Swap";
 
-// getSwapRouteV1();
-// postSwapRouteV1();
-// V1Swap();
+/**
+ * V1 Implementation
+ */
+    // APIs
+        // getSwapRouteV1();
+        // postSwapRouteV1();
+    
+    // Execution
+        // V1Swap();
 
-// getSwapRouteEncodedV0();
-V0Swap();
+
+/**
+ * Non-versioned Implementation
+ */
+
+    // APIs
+        // getSwapRouteEncodedV0();
+
+    // Execution    
+        // V0Swap();
